3dd1d7372a0756d85acfb1bf0cf2933d32061657
Meghan Denny <nektromc@gmail.com>
2019-12-01 18:42:37 -0500

    add insertpost

--- post.go
+++ post.go
@@ -9,3 +9,8 @@ type Post struct {
 	PostJson  string `json:"json" sqlite:"text"`
 	Link      string `json:"link" sqlite:"text"`
 }
+
+func InsertPost(sub, post, title, pjson, link string) {
+	id := db.QueryNextID("posts")
+	db.QueryPrepared(true, "insert into posts values (?, ?, ?, ?, ?, ?)", id, sub, post, title, pjson, link)
+}
