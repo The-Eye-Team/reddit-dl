71fbbdf5dbbe10c10c8bd7308cb6ad9be43042e5
Meghan Denny <nektromc@gmail.com>
2019-12-01 17:35:23 -0500

    add post struct tags

--- post.go
+++ post.go
@@ -1,11 +1,11 @@
 package main
 
 type Post struct {
-	ID        int64 ``
+	ID        int64 `json:"id"`
 	IDS       string
-	Subreddit string ``
-	PostID    string ``
-	Title     string ``
-	Selftext  string ``
-	Link      string ``
+	Subreddit string `json:"subreddit" sqlite:"text"`
+	PostID    string `json:"post_id" sqlite:"text"`
+	Title     string `json:"title" sqlite:"text"`
+	Selftext  string `json:"selftext" sqlite:"text"`
+	Link      string `json:"link" sqlite:"text"`
 }
