cc3a94d1aa3bf28b541ab30fa6da1e9e566738c8
Meghan Denny <nektromc@gmail.com>
2020-02-15 15:47:03 -0500

    build_all - requires cgo=1

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 build_template() {
-    export CGO_ENABLED=0
+    export CGO_ENABLED=1
     export GOOS=$1
     export GOARCH=$2
     export GOARM=7
@@ -13,35 +13,44 @@ build_template() {
     go build -ldflags="-s -w" -o ./bin/reddit-dl-v$tag-$GOOS-$GOARCH$ext
 }
 
+# build_template aix ppc64
+# build_template android 386
+# build_template android amd64
+# build_template android arm
+# build_template android arm64
 # build_template darwin 386
-build_template darwin amd64
-build_template dragonfly amd64
+# build_template darwin amd64
+# build_template darwin arm
+# build_template darwin arm64
+# build_template dragonfly amd64
 # build_template freebsd 386
-build_template freebsd amd64
+# build_template freebsd amd64
 # build_template freebsd arm
+# build_template js wasm
 # build_template linux 386
 build_template linux amd64
 # build_template linux arm
 # build_template linux arm64
-# build_template linux ppc64
-# build_template linux ppc64le
 # build_template linux mips
-# build_template linux mipsle
 # build_template linux mips64
 # build_template linux mips64le
+# build_template linux mipsle
+# build_template linux ppc64
+# build_template linux ppc64le
 # build_template linux s390x
 # build_template nacl 386
 # build_template nacl amd64p32
 # build_template nacl arm
 # build_template netbsd 386
-build_template netbsd amd64
+# build_template netbsd amd64
 # build_template netbsd arm
 # build_template openbsd 386
-build_template openbsd amd64
+# build_template openbsd amd64
 # build_template openbsd arm
 # build_template plan9 386
 # build_template plan9 amd64
 # build_template plan9 arm
-build_template solaris amd64
-# build_template windows 386
-build_template windows amd64
+# build_template solaris amd64
+# build_template windows 386 .exe
+# build_template windows amd64 .exe
+# build_template windows arm .exe
